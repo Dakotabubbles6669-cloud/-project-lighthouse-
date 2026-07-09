a1ab4762c31cab8a885a7220c74b9c67e245abcf
Dakotabubbles6669-cloud <Dakota.bubbles6669@gmail.com>
2026-07-09 00:03:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const intro = document.querySelector("#intro");
+const skipIntro = document.querySelector("#skipIntro");
+
+function closeIntro() {
+  if (!intro) return;
+  intro.classList.add("hidden");
+  sessionStorage.setItem("projectLighthouseIntroSeen", "true");
+}
+
+if (intro) {
+  const alreadySeen = sessionStorage.getItem("projectLighthouseIntroSeen");
+
+  if (alreadySeen) {
+    intro.classList.add("hidden");
+  } else {
+    setTimeout(closeIntro, 4200);
+  }
+}
+
+if (skipIntro) {
+  skipIntro.addEventListener("click", closeIntro);
+}
+
 const cards = document.querySelectorAll(".project-card, .timeline-item, .panel");
 
 const observer = new IntersectionObserver(
@@ -8,9 +31,7 @@ const observer = new IntersectionObserver(
       }
     });
   },
-  {
-    threshold: 0.15,
-  }
+  { threshold: 0.15 }
 );
 
 cards.forEach((card) => {
